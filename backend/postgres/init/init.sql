6433b3cb1b4490b0ae079144d22658ffae2b5728
Kirill Adayev <adayevkp@gmail.com>
2024-02-13 01:15:03 +0300

    add time entries storage implementation

--- backend/postgres/init/init.sql
+++ backend/postgres/init/init.sql
@@ -29,4 +29,5 @@ INSERT into time_entries(
     id, start_time, end_time, project_id
 ) values
 (1, '2024-01-13 12:30', '2024-01-13 15:45', 1),
+(3, '2024-01-13 13:30', '2024-01-13 15:45', 1),
 (2, '2024-01-20 22:30', NULL, NULL);
