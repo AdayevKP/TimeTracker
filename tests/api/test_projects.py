4ffdc143d0cb087d6f133ee226d82b5163e8ade0
AdayevKP <adayevkp@gmail.com>
2024-02-20 23:07:33 +0300

    refactor fixtures

--- tests/api/test_projects.py
+++ tests/api/test_projects.py
@@ -1,23 +1,24 @@
-import typing as tp
-
+# mypy: ignore-errors
 import httpx
 import pytest
+import sqlalchemy
 
 from time_tracker.db import models
 
 
 @pytest.fixture(scope="function")
-async def fill_db(async_db) -> tp.AsyncIterator[None]:  # type: ignore
-    async_db.add(models.Project(name="work", description=None))
-    async_db.add(models.Project(name="study", description="learning math"))
-    await async_db.commit()
+async def fill_db(async_session, reset_primary_key):
+    async_session.add(models.Project(name="work", description=None))
+    async_session.add(
+        models.Project(name="study", description="learning math")
+    )
+    await async_session.commit()
     yield
-    # async_db.execute("ALTER SEQUENCE projects_id_seq RESTART WITH 1")
-    # async_db.commit()
+    await reset_primary_key("projects", "id")
 
 
 @pytest.mark.usefixtures("fill_db")
-async def test_projects_list(client: httpx.AsyncClient) -> None:
+async def test_projects_list(client: httpx.AsyncClient):
     response = await client.get("/projects/")
     assert response.status_code == 200
     assert response.json() == [
@@ -27,20 +28,27 @@ async def test_projects_list(client: httpx.AsyncClient) -> None:
 
 
 @pytest.mark.usefixtures("fill_db")
-async def test_add_project(client: httpx.AsyncClient) -> None:
+async def test_add_project(client: httpx.AsyncClient, async_session):
     response = await client.post(
         "/projects/",
         json={"name": "MEGA PROJECT", "description": "mega description"},
     )
     assert response.status_code == 201
+    assert response.json() == {
+        "name": "MEGA PROJECT",
+        "id": 3,
+        "description": "mega description",
+    }
 
-    # res = await pgsql.execute(sqlalchemy.text("SELECT * FROM projects"))
-    # assert res.fetchall() == [
-    #     {
-    #         'id': 1, 'name': 'MEGA PROJECT',
-    #         'description': 'mega description'
-    #     }
-    # ]
+    db_proj = await async_session.execute(
+        sqlalchemy.text("Select * from projects where id = 3")
+    )
+    proj = db_proj.mappings().first()
+    assert proj == {
+        "name": "MEGA PROJECT",
+        "id": 3,
+        "description": "mega description",
+    }
 
 
 @pytest.mark.usefixtures("fill_db")
@@ -54,7 +62,20 @@ async def test_add_project(client: httpx.AsyncClient) -> None:
         ("2", {"name": "study", "id": 2, "description": "learning math"}),
     ],
 )
-async def test_get_project(client: httpx.AsyncClient, proj_id, expected):  # type: ignore # noqa E501
+async def test_get_project(client: httpx.AsyncClient, proj_id, expected):
     response = await client.get(f"/projects/{proj_id}")
     assert response.status_code == 200
     assert response.json() == expected
+
+
+@pytest.mark.usefixtures("fill_db")
+async def test_update_project(client: httpx.AsyncClient):
+    response = await client.put(
+        "/projects/1", json={"name": "work", "description": "working hard"}
+    )
+    assert response.status_code == 200
+    assert response.json() == {
+        "id": 1,
+        "name": "work",
+        "description": "working hard",
+    }
--- tests/conftest.py
+++ tests/conftest.py
@@ -1,3 +1,4 @@
+# mypy: ignore-errors
 import asyncio
 import typing as tp
 
@@ -6,10 +7,8 @@ import pytest
 import sqlalchemy
 from sqlalchemy.ext import asyncio as sa
 
-# from sqlalchemy.ext.declarative import declarative_base
 from time_tracker import app, config
 from time_tracker.db import deps as db_deps
-from time_tracker.db import models as db_models
 
 
 _SETTINGS = config.get_settings()
@@ -26,40 +25,49 @@ TestingAsyncSessionLocal: sa.async_sessionmaker[
 ] = sa.async_sessionmaker(async_engine, autocommit=False, autoflush=False)
 
 
-async def override_get_session():  # type: ignore
-    async with TestingAsyncSessionLocal() as sess:
-        yield sess
-
-
-app.app.dependency_overrides[db_deps.get_session] = override_get_session
-
-
-# @pytest.fixture(scope='session')
-# async def async_db_engine():
-#     async with async_engine.begin() as conn:
-#         await conn.run_sync(db_models.Base.metadata.drop_all)
-#         await conn.run_sync(db_models.Base.metadata.create_all)
-
-
-# truncate all table to isolate tests
 @pytest.fixture(scope="function")
-async def async_db():  # type: ignore
-    async with TestingAsyncSessionLocal() as session:
-        await session.begin()
-        yield session
-        await session.rollback()
+@pytest.mark.usefixtures("reset_primary_key")
+async def async_session(reset_primary_key):
+    conn = await async_engine.connect()
+    transaction = conn.begin()
+    await transaction.start()
 
-        for table in db_models.Base.metadata.tables.values():
-            print(table)
-            await session.execute(
-                sqlalchemy.text(f"TRUNCATE TABLE {table} cascade")
+    session = TestingAsyncSessionLocal(
+        bind=conn, join_transaction_mode="create_savepoint"
+    )
+    yield session
+    await session.commit()
+    await session.close()
+
+    # rollback - everything that happened with the
+    # Session above (including calls to commit())
+    # is rolled back.
+    await transaction.rollback()
+    # return connection to the Engine
+    await conn.close()
+
+
+@pytest.fixture(scope="function")
+async def reset_primary_key():
+    reset_name = []
+
+    async def setter(table_name, pk_name):
+        reset_name.append((table_name, pk_name))
+
+    yield setter
+
+    async with TestingAsyncSessionLocal() as sess:
+        for t, pk in reset_name:
+            sql = sqlalchemy.text(
+                f"ALTER SEQUENCE {t}_{pk}_seq RESTART WITH 1; "  # noqa: E702
             )
-        await session.commit()
+            await sess.execute(sql)
+        await sess.commit()
 
 
 # let test session to know it is running inside event loop
 @pytest.fixture(scope="session")
-def event_loop():  # type: ignore
+def event_loop():
     policy = asyncio.get_event_loop_policy()
     loop = policy.new_event_loop()
     yield loop
@@ -72,7 +80,14 @@ async def engine_conn() -> tp.AsyncGenerator[sa.AsyncConnection, None]:
         yield conn
 
 
-@pytest.fixture(scope="session")
-async def client() -> tp.AsyncIterator[httpx.AsyncClient]:
+@pytest.fixture(scope="function")
+async def client(async_session) -> tp.AsyncIterator[httpx.AsyncClient]:
+    async def override_get_session():  # type: ignore
+        yield async_session
+
+    app.app.dependency_overrides[db_deps.get_session] = override_get_session
+
     async with httpx.AsyncClient(app=app.app, base_url="http://test") as ac:
         yield ac
+
+    del app.app.dependency_overrides[db_deps.get_session]
